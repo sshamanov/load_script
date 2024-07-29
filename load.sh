f28628561c99a6a9437f64adde463da7cdbbfffe
Sergei Shamanov <sshamanov@ymail.com>
2024-07-29 18:39:15 +0300

    Create load.sh

---
+++ load.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+#Vars
+
+SETTINGS=$HOME/.config/load.env
+SCRIPT=$HOME/bin/load.sh
+URL="https://gist.githubusercontent.com/sshamanov/db385e34e61ca90b67f07497b036cc8b/raw/8319af598d7b4b69c000038300876ca5907472c7/load.sh"
+
+#Functions
+
+value ()
+{
+	if echo $2 | grep -q -E "^[0-9]+-[0-9]+$" ; then
+		if [ $1 = min ]; then
+			echo $(echo $2 | awk -F - '{print$1}')
+		elif [ $1 = max ]; then
+                        echo $(echo $2 | awk -F - '{print$2}')
+		else
+			exit 1
+		fi
+	elif echo $2 | grep -q -E "^[0-9]+$" ; then
+		echo $2
+	else
+		echo "Faild to parse values!"
+                echo "Please enter value using format MIN-MAX or CUR"
+                echo "Where MIN, MAX or CUR percent values without % sign"
+                exit 1
+	fi
+}
+
+parse_settings()
+{
+	while [[ $# -gt 2 ]]; do
+		case $2 in
+			-c|--cpu)
+				CPU0=$(value min $3)
+				CPU1=$(value max $3)
+				shift
+				shift
+				;;
+			-m|--mem)
+				MEM0=$(value min $3)
+				MEM1=$(value max $3)
+				shift
+				shift
+				;;
+			-d|--hdd)
+				HDD0=$(value min $3)
+				HDD1=$(value max $3)
+				shift
+				shift
+				;;
+			*)
+				echo "Unknown options!"
+				exit 1
+				;;
+		esac
+	done
+}
+
+save_settings()
+{
+	cat <<EOF > $SETTINGS
+CPU0=$CPU0
+CPU1=$CPU1
+MEM0=$MEM0
+MEM1=$MEM1
+HDD0=$HDD0
+HDD1=$HDD1
+EOF
+}
+
+save_scheduler()
+{
+	crontab -u $USER -r
+	echo "* * * * * $SCRIPT run > /dev/null" | crontab -u $USER -
+	
+}
+
+install_script()
+{
+	which stress-ng > /dev/null || ( sudo apt update ; sudo apt -y install stress-ng )
+	which wget > /dev/null || ( sudo apt update ; sudo apt -y wget )
+	mkdir -p $HOME/bin
+	wget $URL -O $SCRIPT
+}
+
+# Start from here
+
+if [ ! -f $SETTINGS ] ; then
+	echo "First run."
+	echo "Empty config!"
+	echo "Please run with $0 install [-c|--cpu 0-100] [-m|--mem 0-100] [-d|--hdd 0-100]"
+	exit 1
+else
+	echo "Load setting from $SETTINGS"
+fi
+
+# Commands
+
+case $1 in
+	install)
+		echo "Instaling..."
+		install_script
+		parse_settings $@
+		save_settings
+		save_scheduler
+		;;
+	run|start)
+		echo "Starting..."
+		load_settings
+		;;
+	delete|remove|stop|disable)
+		echo "Deleting script..."
+		uninstall
+		;;
+	*)
+		echo "Unknown command!"
+		echo "Please use $0 [install|run|delete] [options]"
+		exit 1
+		;;
+esac
