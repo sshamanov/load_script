517f180bb0aa0c83ff457f307f6c79bd35777231
Sergei Shamanov <sshamanov@ymail.com>
2024-07-29 19:51:30 +0300

    Update load.sh

--- load.sh
+++ load.sh
@@ -5,6 +5,8 @@
 SETTINGS=$HOME/.config/load.env
 SCRIPT=$HOME/bin/load.sh
 URL="https://raw.githubusercontent.com/sshamanov/load_script/main/load.sh"
+OPTIONS='Please run with $0 install [-c|--cpu 0-100] [-m|--mem 0-100] [-d|--hdd 0-100]'
+HDDPATH="$HOME/temp-hdd-load"
 
 #Functions
 
@@ -30,6 +32,11 @@ value ()
 
 parse_settings()
 {
+	test $# -gt 2 || (
+		echo "Empty settings"
+		echo $OPTIONS
+		exit 1 )
+
 	while [[ $# -gt 2 ]]; do
 		case $2 in
 			-c|--cpu)
@@ -52,6 +59,7 @@ parse_settings()
 				;;
 			*)
 				echo "Unknown options!"
+				echo $OPTIONS
 				exit 1
 				;;
 		esac
@@ -82,8 +90,78 @@ install_script()
 	which stress-ng > /dev/null || ( sudo apt update ; sudo apt -y install stress-ng )
 	which wget > /dev/null || ( sudo apt update ; sudo apt -y wget )
 	mkdir -p $HOME/bin
-	wget $URL -O $SCRIPT
- 	chmod +x $SCRIPT
+	test -x $SCRIPT || wget $URL -O $SCRIPT
+}
+
+rund_val()
+{
+	VAL0=$1
+	VAL1=$2
+	if [ ! -z $VAL0 ] && [ ! -z $VAL1 ] && [ $VAL1 -gt $VAL0 ] ; then
+                VAL=$(( VAL0 + RANDOM % (VAL1 - VAL0) ))
+        else
+                VAL=$VAL0
+        fi
+}
+
+run()
+{
+	echo "Load settings..."
+	. $SETTINGS
+
+	rund_val $HDD0 $HDD1
+	HDD=$VAL
+
+	rund_val $CPU0 $CPU1
+	CPU=$VAL
+
+	rund_val $MEM0 $MEM1
+	MEM=$VAL
+
+	if [ ! -z $HDD ] ; then
+		echo "Starting HDD load with $HDD%..."
+		hdd_task &
+	fi
+
+	if [ ! -z $CPU ] && [ -z $MEM ] ; then
+                echo "Starting CPU load with $CPU%..."
+		cpu_task &
+        fi
+
+        if [ ! -z $MEM ] && [ -z $CPU ] ; then
+                echo "Starting MEM load with $MEM%..."
+		mem_task &
+        fi
+
+        if [ ! -z $CPU ] && [ ! -z $MEM ] ; then
+                echo "Starting CPU load with $CPU%..."
+                echo "Starting MEM load with $MEM%..."
+		cpu_mem_task &
+        fi
+}
+
+hdd_task(){
+	rm -rf $HDDPATH/file*
+	mkdir -p $HDDPATH
+	until [ $(df / | tail -n1 | awk '{print$5}' | tr -d %) -gt $((HDD - 1)) ] ; do
+		fallocate --length 1G $HDDPATH/file.$((1 + RANDOM % 1000))
+	done
+}
+
+cpu_val(){
+	VAL=$((CPU * $(getconf _NPROCESSORS_ONLN)))
+}
+
+cpu_task(){
+	stress-ng --cpu $(getconf _NPROCESSORS_ONLN) --cpu-load $CPU --timeout 60
+}
+
+mem_task(){
+	systemd-run --scope -p CPUQuota=50% stress-ng --vm 1 --vm-keep --vm-bytes $MEM% --timeout 60
+}
+
+cpu_mem_task(){
+	systemd-run --scope -p CPUQuota=$((CPU * $(getconf _NPROCESSORS_ONLN)))% stress-ng --cpu $(getconf _NPROCESSORS_ONLN) --vm 1 --vm-keep --vm-bytes $MEM% --timeout 60
 }
 
 # Start from here
@@ -91,10 +169,8 @@ install_script()
 if [ ! -f $SETTINGS ] ; then
 	echo "First run."
 	echo "Empty config!"
-	echo "Please run with $0 install [-c|--cpu 0-100] [-m|--mem 0-100] [-d|--hdd 0-100]"
+	echo $OPTIONS
 	exit 1
-else
-	echo "Load setting from $SETTINGS"
 fi
 
 # Commands
@@ -106,14 +182,16 @@ case $1 in
 		parse_settings $@
 		save_settings
 		save_scheduler
+		echo "Instlled with setings:"
+		cat $SETTINGS
 		;;
 	run|start)
 		echo "Starting..."
-		load_settings
+		run
 		;;
 	delete|remove|stop|disable)
-		echo "Deleting script..."
-		uninstall
+		echo "Deleting schedule..."
+		crontab -u $USER -r
 		;;
 	*)
 		echo "Unknown command!"
